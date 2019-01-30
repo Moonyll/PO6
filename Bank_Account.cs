a103d02a6fc6ff9fe367ac03e02574d16ca09c1f
Moonyll <s.pouwel@laposte.net>
2019-01-30 15:18:27 +0100

    PO6

---
+++ Bank_Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO6
+{
+    class Bank_Account
+    {
+        public string _owner;
+        public string _currency;
+        public double _final_settlement;
+        public void _tocredit(double _credit)
+        {
+        _final_settlement = _credit;
+        }
+        public void _todebit(double _debit)
+        {
+        _final_settlement = _debit;
+        }
+    }
+}
